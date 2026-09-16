ec9fc0757307d5a05e6764ace126f005b51616ee
Rudy Adalid Osorio Sosa <rudy_adalid@unitec.edu>
2026-09-15 22:17:12 -0600

    Restaurante Mi Casita Proyecto

--- RestaurateMiCasita/src/restauratemicasita/RestaurateMiCasita.java
+++ RestaurateMiCasita/src/restauratemicasita/RestaurateMiCasita.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template 
 */
 
-/* @author UserCP */
+/* @author UserCP ] Rudy | Rafael */
 
 package restauratemicasita;
 import java.util.Scanner;
